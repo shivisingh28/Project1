49f0f51d0e7daa20f2053159fed108dd962fc3f7
shivisingh28 <shivi2819997@gmail.com>
2019-06-18 14:37:56 +0530

    first commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function showProgress(){
 	var currentQuestionNumber = quiz.questionIndex + 1;
 	var element = document.getElementById("progress");
 	element.innerHTML="Question" + currentQuestionNumber +" of "+ quiz.questions.length;
-}
+};
 function showScores() {
 	var gameOverHtml = "<h1>Result</h1>";
 	gameOverHtml +="<h2 id='score'> Your scores: " + quiz.score + "</h2>";
@@ -36,11 +36,13 @@ function showScores() {
 };
 var questions =[
         new Question("Breadth First Search is used in?",["Binary Trees","Graphs","Stacks","Both a and b"],"Both a and b"),
-                      new Question("Which language is used for styling web pages?",["HTML","JQuery","CSS","XML"],"CSS"),
-                      new Question("Which protocol is used to receive e-mail?",["SMTP","POP3","HTTP","FTP"],"POP3"),
-                      new Question("Which protocol is used to send e-mail?",["HTTP","POP3","SMTP","SSH"],"SMTP"),
-                     new Question("The simplest CPU scheduling algorithm is __________?",["Multilevel scheduling algorithm","FCFS scheduling algorithm","SJF scheduling algorithm","Round Robin scheduling algorithm"],"FCFS scheduling algorithm"),
-                 new Question("The GUI means?",["General User Interaction","Graphical User Interface","Guided User Interface","General User Interface"],"Graphical User Interface"),
+        new Question("Which language is used for styling web pages?",["HTML","JQuery","CSS","XML"],"CSS"),
+        new Question("Which protocol is used to receive e-mail?",["SMTP","POP3","HTTP","FTP"],"POP3"),
+        new Question("Which protocol is used to send e-mail?",["HTTP","POP3","SMTP","SSH"],"SMTP"),
+        new Question("The simplest CPU scheduling algorithm is __________?",["Multilevel scheduling algorithm","FCFS scheduling algorithm","SJF scheduling algorithm","Round Robin scheduling algorithm"],"FCFS scheduling algorithm"),
+        new Question("The GUI means?",["General User Interaction","Graphical User Interface","Guided User Interface","General User Interface"],"Graphical User Interface")
 ];
+//create Quiz
 var quiz = new Quiz(questions);
+//display quiz
 populate();
